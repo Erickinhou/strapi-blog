4b5c0fb25f25102189baf670164fbaad0ba5f3ee
Erickinhou <willianerick17@gmail.com>
2022-05-05 18:24:02 -0300

    add home header

---
+++ components/HomeHeader.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function HomeHeader() {
+  return (
+    <header>
+      <h1>Devistry Blog</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque, cum
+        nihil explicabo quod rem hic quaerat dolores numquam eligendi
+        necessitatibus perferendis consectetur esse a, blanditiis, eius nesciunt
+        asperiores facilis. Rem!
+      </p>
+    </header>
+  );
+}
+
+export default HomeHeader;
